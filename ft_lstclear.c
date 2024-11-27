9bf12ba7ecabda1e4d112a9d067315e24c4cbfad
Chizuru <chizuru.yamauchi@dci-student.org>
2024-11-27 14:59:45 +0100

    add business logic in linked list c files

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -14,5 +14,51 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
+	t_list	*temp;
+	if (!lst || !del)
+		return;
+	while (*lst)
+	{
+		temp = (*lst)->next;
+		del((*lst)->content);
+		free(*lst);
+		*lst = temp;
+	}
+	*lst= NULL;
+}
+
+void	del_string(void *content)
+{
+	free(content);
+}
+
+#include <stdio.h>
+
+int	main()
+{
+	char *str1 = ft_strdup("first");
+	char *str2 = ft_strdup("second");
+	char *str3 = ft_strdup("third");
 	
+	t_list *node1 = ft_lstnew(str1);
+	t_list *node2 = ft_lstnew(str2);
+	t_list *node3 = ft_lstnew(str3);
+	
+	node1->next = node2;
+	node2->next = node3;
+	
+	printf("Before clearing:\n");
+	t_list *current = node1;
+	while (current)
+	{
+		printf("%s -> ", (char *)current->content);
+		current = current->next;
+	}
+	printf("NULL\n");
+	ft_lstclear(&node1, del_string);
+	printf("After clearing:\n");
+	if (node1 == NULL)
+	{
+		printf("List is now empty and cleared.\n");
+	}
 }
--- libft_test_each_function.c
+++ libft_test_each_function.c
@@ -542,3 +542,40 @@ int	main()
 	ft_lstdelone(node, del_string);
 	return 0;
 }*/
+
+/*
+void	del_string(void *content)
+{
+	free(content);
+}
+
+#include <stdio.h>
+
+int	main()
+{
+	char *str1 = ft_strdup("first");
+	char *str2 = ft_strdup("second");
+	char *str3 = ft_strdup("third");
+	
+	t_list *node1 = ft_lstnew(str1);
+	t_list *node2 = ft_lstnew(str2);
+	t_list *node3 = ft_lstnew(str3);
+	
+	node1->next = node2;
+	node2->next = node3;
+	
+	printf("Before clearing:\n");
+	t_list *current = node1;
+	while (current)
+	{
+		printf("%s -> ", (char *)current->content);
+		current = current->next;
+	}
+	printf("NULL\n");
+	ft_lstclear(&node1, del_string);
+	printf("After clearing:\n");
+	if (node1 == NULL)
+	{
+		printf("List is now empty and cleared.\n");
+	}
+}*/
