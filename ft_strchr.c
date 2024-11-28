830f5f68f1876a7e81490ef35f0c7e37ecf333e3
Chizuru <chizuru.yamauchi@dci-student.org>
2024-11-28 20:12:00 +0100

    modify ft_strnstr.c file

--- ft_strchr.c
+++ ft_strchr.c
@@ -17,17 +17,19 @@ char	*ft_strchr(const char *s, int c)
 	int	i;
 
 	i = 0;
-	if (c == '\0')
+	if (!s)
 	{
-		return ((char *)(s + ft_strlen(s)));
+		return (NULL);
 	}
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
+		if (s[i] == (char)c)
 		{
 			return ((char *)(s + i));
 		}
 		i++;
 	}
+	if (s[i] == (char)c)
+		return ((char *)(s + i));
 	return (NULL);
 }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,16 +14,24 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t	little_len;
+	size_t	j;
 	size_t	i;
 
-	little_len = ft_strlen(little);
 	i = 0;
-	if (little_len == 0)
+	if (!big && !little)
+		return (NULL);
+	if (little[0] == '\0' || little == big)
 		return ((char *)big);
-	while (i <= len - little_len)
+	while (big[i])
 	{
-		if (ft_strncmp(big + i, little, little_len) == 0)
+		j = 0;
+		while (big[i + j] == little[j] && (i + j) < len)
+		{
+			if (big[i + j] == '\0' && little[j] == '\0')
+				return ((char *)&big[i]);
+			j++;
+		}
+		if (little[j] == '\0')
 			return ((char *)(big + i));
 		i++;
 	}
